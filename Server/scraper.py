4555bd22df23af0921361f7eb0a07a972eb04257
Donal Burke <dbrke98@gmail.com>
2018-02-28 21:37:31 +0000

    Got scraper to populate timetables into a mongo database, finished for now

--- Server/scraper.py
+++ Server/scraper.py
@@ -21,9 +21,8 @@ class ArchitectFinderScraper(object):
 
         for link in s.findAll("a", text="Academic Year 2017/18"):
             if 'href' in link.attrs: 
-               
                 self.url = link.attrs['href']
-        
+
         self.goto_form_page()
 
     def goto_form_page(self):
@@ -83,10 +82,74 @@ class ArchitectFinderScraper(object):
 
         resp = self.br.response().read()
 
-        self.br.back()
+        self.scrape_timetable(item)
 
+        self.br.back()
+    
     def scrape(self):
-        self.goto_programmes_page()            
+        self.goto_programmes_page()
+
+    def scrape_timetable(self, item):
+        db = client['timetables']
+
+        soup = BeautifulSoup(self.br.response().read())
+
+        list_modules = []
+
+        days = soup.findAll('span', {'class': 'labelone'})
+
+        for num, table in enumerate(soup.findAll('table', {'class', 'spreadsheet'})):
+            day = days[num].get_text()
+            for tr in table.findAll('tr')[1:]:
+                cells = tr.findAll('td')
+                index = 0
+                for i in range(len(cells)):
+                    if i == 0:
+                        moduleName = cells[i].get_text()
+                    if i == 1:
+                        moduleType = cells[i].get_text()
+                    if i == 2:
+                        startTime = cells[i].get_text()
+                    if i == 3:
+                        endTime = cells[i].get_text()
+                    if i == 4:
+                        duration = cells[i].get_text()
+                    if i == 5:
+                        weeks = cells[i].get_text()
+                    if i == 6:
+                        room = cells[i].get_text()
+                    if i == 7:
+                        lecturer = cells[i].get_text()
+                    index = index + 1
+
+                module = {
+                    "day": day, 
+                    "moduleName": moduleName,
+                    "moduleType": moduleType,
+                    "startTime": startTime,
+                    "endTime": endTime,
+                    "duration": duration,
+                    "weeks": weeks,
+                    "room": room,
+                    "lecturer": lecturer
+                }
+
+                list_modules.append(module)
+
+        timetables = list_modules
+
+        jsonObj = {
+            "_id": item.name,
+            "courseName": item.attrs['label'],
+            "timetable" : timetables
+        }
+
+        courses = db.courses
+
+        result = courses.insert_one(jsonObj)
+
+        print('One post: {0}'.format(result.inserted_id))
+            
 
 if __name__ == '__main__':
     scraper = ArchitectFinderScraper()
