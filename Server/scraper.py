8ad938d7893042ee7e0153b53affa53402f05506
Donal Burke <dbrke98@gmail.com>
2018-02-28 16:47:56 +0000

    Added ASP.NET form requests so the scraper can goto the timetable page for each programme in the college

--- Server/scraper.py
+++ Server/scraper.py
@@ -21,8 +21,70 @@ class ArchitectFinderScraper(object):
 
         for link in s.findAll("a", text="Academic Year 2017/18"):
             if 'href' in link.attrs: 
+               
                 self.url = link.attrs['href']
+        
+        self.goto_form_page()
+
+    def goto_form_page(self):
+        self.br.open(self.url)
+        s = BeautifulSoup(self.br.response().read())
+
+        saved_form = s.find('form', id='form1').prettify()
+
+        view_state = s.find('input', id='__VIEWSTATE')
+        view_state_generator = s.find('input', id='__VIEWSTATEGENERATOR')
+        event_validation = s.find('input', id='__EVENTVALIDATION')
+
+        self.br.select_form('form1')
+        self.br.form.set_all_readonly(False)
+        self.br.form['__EVENTTARGET'] = "LinkBtn_programmesofstudy"
+        self.br.form['__VIEWSTATE'] = view_state['value']
+        self.br.form['__VIEWSTATEGENERATOR'] = view_state_generator['value']
+        self.br.form['__EVENTVALIDATION'] = event_validation['value']
+        self.br.form['tLinkType'] = "information"
+        self.br.form.fixup()
+
+        self.br.submit()
+
+        s = BeautifulSoup(self.br.response().read())
+        self.br.select_form('form1')
+        items = self.br.form.find_control('dlObject').get_items()
+
+        for item in items:
+            if len(item.name) < 1:
+                continue
+
+            print('Scraping timetable for course: \n([ %s ])' % item.name)
+            self.scrape_course_timetables(item)
     
+    
+    def scrape_course_timetables(self, item):
+        s = BeautifulSoup(self.br.response().read())
+        saved_form = s.find('form', id='form1').prettify()
+
+        view_state = s.find('input', id='__VIEWSTATE')
+        view_state_generator = s.find('input', id='__VIEWSTATEGENERATOR')
+        event_validation = s.find('input', id='__EVENTVALIDATION')
+        list_view = s.find('option', text="List Timetable")
+
+        self.br.select_form('form1')
+        self.br.form.set_all_readonly(False)
+        self.br.form['__VIEWSTATE'] = view_state['value']
+        self.br.form['__VIEWSTATEGENERATOR'] = view_state_generator['value']
+        self.br.form['__EVENTVALIDATION'] = event_validation['value']
+        self.br.form['tLinkType'] = "programmesofstudy"
+        self.br.form['dlObject'] = [ item.name ]
+        self.br.form['dlType'] = ["TextSpreadsheet;swsurl;SWSCUST Object TextSpreadsheet", ]
+
+        self.br.form.fixup()
+
+        self.br.submit(name='bGetTimetable')
+
+        resp = self.br.response().read()
+
+        self.br.back()
+
     def scrape(self):
         self.goto_programmes_page()            
 
