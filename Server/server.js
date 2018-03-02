76fe67f451c5f489a0ff4bc9dac791e36ecb37ad
nakster <g00332403@gmit.ie>
2018-03-02 20:02:45 +0000

    headers/rquest logger/port

--- Server/server.js
+++ Server/server.js
@@ -15,3 +15,20 @@ mongoose.connect('mongodb://localhost/timetables', function (error) {
     }
 });
 
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({ 'extended': 'true' }));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
+app.use(cors());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// listen (start app with node server.js) ======================================
+app.listen(8081);
+console.log("App listening on port 8081");
