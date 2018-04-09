4f39c1fa2d1788f9a0fc745204a693553a44429d
nakster <g00332403@gmit.ie>
2018-04-09 19:15:16 +0100

    ionViewDidLoad changed in login.ts

--- App/src/pages/login/login.ts
+++ App/src/pages/login/login.ts
@@ -18,11 +18,27 @@ import { SignupPage } from '../signup/signup';
 })
 export class LoginPage {
 
+  //variables
+  email: string;
+  password: string;
+  loading: any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public authService: AuthProvider, public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+    //this.showLoader();
+    //Check if already authenticated
+    this.authService.checkAuthentication().then((res) => {
+        console.log("Already authorized");
+        this.loading.dismiss();
+        this.navCtrl.setRoot(HomePage);
+    }, (err) => {
+        console.log("Not already authorized");
+        this.loading.dismiss();
+    });
   }
+  
 
 }
